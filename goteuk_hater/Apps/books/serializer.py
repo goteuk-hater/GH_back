2571ba38e96f5ef07402303b1600bed06ae9875d
SungChul Hong <hsc6263@gmail.com>
2023-09-02 13:10:10 +0900

    set serializer

--- goteuk_hater/Apps/books/serializer.py
+++ goteuk_hater/Apps/books/serializer.py
@@ -13,9 +13,9 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
-    # def to_representation(self, instance):
-    #     self.fields['category'] = CategoryRepresentationSerializer(read_only=True)
-    #     return super(BookSerializer, self).to_representation(instance)
+    def to_representation(self, instance):
+        self.fields['category'] = CategoryRepresentationSerializer(read_only=True)
+        return super(BookSerializer, self).to_representation(instance)
 
 class CategoryRepresentationSerializer(serializers.ModelSerializer):
     class Meta:
