8dd2bc5511721867306f1a0d0984e31a7be6c48d
SungChul Hong <hsc6263@gmail.com>
2023-09-02 15:39:56 +0900

    set ignore and login url

--- goteuk_hater/Apps/user/urls.py
+++ goteuk_hater/Apps/user/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('user_list', UserListCreateAPI.as_view()),
     path('<int:pk>', UserUpdateDestroyAPI.as_view()),
+    path('login', UserLoginAPI.as_view()),
 ]
